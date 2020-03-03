0839b376f35496d16b2630585a972a786e94f92b
Helene Behrens <helenerandi@gmail.com>
2020-03-03 18:33:52 +0100

    med plot, rart resultat

--- Project 2/A4.R
+++ Project 2/A4.R
@@ -3,10 +3,13 @@ library(ggplot2)
 
 #install.packages("invgamma")
 library(invgamma)
+
+#install.packages("tidyverse")
+library(tidyverse)
   
 data(coal)
 
-coal.df <- coal #du kan nok bare hente ting rett ut fra coal 
+set.seed(124)
 
 Alg.4 <- function(n,t1.0,l0.0,l1.0,beta.0, obs, sigma){
   # initializing parameters with prior estimates
@@ -18,8 +21,8 @@ Alg.4 <- function(n,t1.0,l0.0,l1.0,beta.0, obs, sigma){
   t2 <- tail(coal$date,1)
   
   theta["t1",1] <- t1.0
-  theta["y0",1] <- max(which(coal$date < t1)) - 1 # disasters before t1, subtracting the first element which is not a disaster
-  theta["y1",1] <- length(coal$date) - y0 - 2 # disasters at and after t1, subtracting start time and end time element
+  theta["y0",1] <- max(which(coal$date < theta["t1",1])) - 1 # disasters before t1, subtracting the first element which is not a disaster
+  theta["y1",1] <- length(coal$date) - theta["y0",1] - 2 # disasters at and after t1, subtracting start time and end time element
   
   theta["l0",1] <- l0.0
   theta["l1",1] <- l1.0
@@ -30,16 +33,24 @@ Alg.4 <- function(n,t1.0,l0.0,l1.0,beta.0, obs, sigma){
     theta["beta",i] <- rinvgamma(1,shape=4, scale=theta["l0",i-1]+ theta["l1",i-1] + 1)
     
     # sample l0 from gamma
-    theta["l0",i] <- rgamma(1,shape=theta["y0",i-1] + 2, scale=1/(theta["t1",i-1] - t0 + 1/theta["beta",i]))
+    theta["l0",i] <- rgamma(1,shape=theta["y0",i-1] + 2, scale=1/(theta["t1",i-1] - t0 + 1/theta["beta",i-1]))
     
     # sample l1 from gamma
-    theta["l1",i] <- rgamma(1,shape = theta["y1",i-1] + 2, scale=1/(t2 - theta["t1",i-1] + 1/theta["beta",i]))
+    theta["l1",i] <- rgamma(1,shape = theta["y1",i-1] + 2, scale=1/(t2 - theta["t1",i-1] + 1/theta["beta",i-1]))
     
     # MH-step; sample t1 (f.ex. from normal with mean t1)
   
-    y <- rnorm(1,t1,sigma)                # proposal for t1
+    y <- rnorm(1,theta["t1",i-1],sigma)                # proposal for t1
+    if (y > t2){
+      cat("For høy y!")
+      y <- t2
+    }
+    else if (y < t0){
+      cat("for lav y1")
+      y <- t0
+    }
     u <- runif(1)        
-    lg.alpha <- min(0, (theta["l1",i]-theta["l0",i])*(y - theta["t1",i-1]))  # log acceptance probability
+    lg.alpha <- min(0, (theta["l1",i-1]-theta["l0",i-1])*(y - theta["t1",i-1]))  # log acceptance probability
     alpha = exp(lg.alpha)                 # acceptance probability
     if (u < alpha){
       theta["t1",i] <- y
@@ -47,16 +58,58 @@ Alg.4 <- function(n,t1.0,l0.0,l1.0,beta.0, obs, sigma){
     else{
       theta["t1",i] <- theta["t1",i-1]
     }
-    # update y0 based on t1 and obs
-    theta["y0",i] <- max(which(coal$date < theta["t1",i])) - 1
-    # update y1 based on t1 and obs
+    # update y0 based on current t1 and obs
+    theta["y0",i] <- max(which(coal$date <= theta["t1",i])) - 1
+    # update y1 based on current t1 and obs
     theta["y1",i] <- length(coal$date) - theta["y0",i] - 2
   }
   return(theta)
 }
 
-theta.test <- Alg.4(50,1870,5,1,3,coal,15)
+test <- Alg.4(5,1870,5,1,3,coal,15)
+test.df <- as.data.frame(t(test))
 
+theta <- Alg.4(10000,1930,5,1,3,coal,5)
+theta.df <- as.data.frame(t(theta))
+
+t1.mean <- mean(theta.df[100:length(theta.df$t1),]$t1)
+t1.mean
+
+hist <- ggplot(theta.df,aes(x=t1)) + geom_histogram(binwidth = 1)
+hist <- hist + geom_vline(xintercept = t1.mean, col="red")
+hist
+
+time.proc <- ggplot(theta.df,aes(x=seq(1,length(t1),1),y=t1)) + geom_line()
+time.proc
+
+l0.proc <- ggplot(theta.df,aes(x=seq(1,length(t1),1),y=l0)) + geom_line()
+l0.proc
+
+l1.proc <- ggplot(theta.df,aes(x=seq(1,length(t1),1),y=l1)) + geom_line()
+l1.proc
+
+beta.proc <- ggplot(theta.df,aes(x=seq(1,length(t1),1),y=beta)) + geom_line()
+beta.proc
+
+l0.mean <- mean(theta.df[100:length(theta.df$t1),]$l0)
+l0.mean
+
+l1.mean <- mean(theta.df[100:length(theta.df$t1),]$l1)
+l1.mean
+
+#values in helplines:
+x0 <- coal$date[1]
+yend0 <- l0.mean*(t1.mean - x0)
+xend1 <- tail(coal$date,1)
+yend1 <- l1.mean*(xend1 - t1.mean) + yend0
+
+
+compare <- ggplot() + geom_point(data=coal, aes(x=date,y=seq(1,length(date),1)))
+compare <- compare + geom_segment(aes(x=x0, xend=t1.mean, y = 0, yend = yend0))
+compare <- compare + geom_segment(aes(x = t1.mean, xend=xend1, y = yend0, yend = yend1))
+compare
+    
+                                  
 
 
 
